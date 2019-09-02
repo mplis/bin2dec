e36ad04418483794d614d56d22afccc1f2303fdf
Mike Plis <michael.plis01@gmail.com>
2019-09-02 17:27:57 -0400

    Implement app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+function binaryToDecimal(binaryString) {
+    return binaryString
+        .split('')
+        .reverse() // reverse the string since the user is inputting digits from the right
+        .reduce((decimal, digit, index) => {
+            return digit === '1' ? decimal + Math.pow(2, index) : decimal;
+        }, 0);
+}
+
+const binaryRegex = RegExp('^[0-1]*$');
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [binaryString, setBinaryString] = React.useState('');
+    const [error, setError] = React.useState(null);
+
+    const decimalString = binaryToDecimal(binaryString);
+
+    function handleChange(e) {
+        const value = e.target.value;
+        if (!binaryRegex.test(value)) {
+            setError("Input must contain only 0's or 1's");
+        } else {
+            setError(null);
+        }
+        setBinaryString(value.replace(/[^0-1]/gi, ''));
+    }
+
+    return (
+        <div style={{ margin: '1rem' }}>
+            <h1>Bin2Dec</h1>
+            <h2>Binary</h2>
+            <input placeholder="Enter 0's or 1's" value={binaryString} onChange={handleChange} />
+            {error ? <div style={{ color: 'red' }}>{error}</div> : null}
+            <h2>Decimal</h2>
+            <div>{decimalString}</div>
+        </div>
+    );
 }
 
 export default App;
